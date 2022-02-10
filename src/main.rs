eb39b209294fca3e009d3c4a6b7a17aab9565668
rewine <lhongxu@outlook.com>
2022-02-10 22:24:28 +0800

    impl: hibernate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io::{ Error, ErrorKind};
-use std::process::Command;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::{Error, ErrorKind};
+use std::process::Command;
 
 use glib::clone;
 use gtk::gdk_pixbuf::ffi::GdkPixbufLoader;
@@ -10,14 +10,10 @@ use gtk::glib;
 use gtk::prelude::*;
 extern crate system_shutdown;
 use system_shutdown::ShutdownResult;
-use system_shutdown::{shutdown, reboot};
-
+use system_shutdown::{reboot, shutdown};
 
 fn main() {
-    let application = gtk::Application::new(
-        Some("com.github.rew-shutdown"),
-        Default::default(),
-    );
+    let application = gtk::Application::new(Some("com.github.rew-shutdown"), Default::default());
 
     application.connect_activate(build_ui);
     application.run();
@@ -45,7 +41,25 @@ pub fn rew_logout() -> ShutdownResult {
 }
 
 pub fn rew_hibernate() -> ShutdownResult {
-
+    let mut cmd = Command::new("dbus-send");
+    cmd.arg("--system")
+        .arg("--print-reply")
+        .arg("--dest=org.freedesktop.login1")
+        .arg("/org/freedesktop/login1")
+        .arg("org.freedesktop.login1.Manager.Suspend")
+        .arg("boolean:true");
+    match cmd.output() {
+        Ok(output) => {
+            if output.status.success() && output.stderr.is_empty() {
+                return Ok(());
+            }
+            Err(Error::new(
+                ErrorKind::Other,
+                String::from_utf8(output.stderr).unwrap(),
+            ))
+        }
+        Err(error) => Err(error),
+    }
 }
 
 fn build_ui(application: &gtk::Application) {
@@ -68,30 +82,36 @@ fn build_ui(application: &gtk::Application) {
     // Add the grid in the window
     window.set_child(Some(&grid));
 
-    let button_1 = gtk::Button::with_label("logout");
-    button_1.connect_clicked(move |_| match rew_logout() {
+    let button_logout = gtk::Button::with_label("logout");
+    button_logout.connect_clicked(move |_| match rew_logout() {
         Ok(_) => println!("Logout, bye!"),
         Err(error) => eprintln!("Failed to logout: {}", error),
     });
 
-    grid.attach(&button_1, 0, 0, 1, 1);
+    grid.attach(&button_logout, 0, 0, 1, 1);
 
-    let button_2 = gtk::Button::with_label("shutdown");
-    button_2.connect_clicked(move |_|  match shutdown() {
+    let button_shutdown = gtk::Button::with_label("shutdown");
+    button_shutdown.connect_clicked(move |_| match shutdown() {
         Ok(_) => println!("Shutting down, bye!"),
         Err(error) => eprintln!("Failed to shut down: {}", error),
     });
 
-    grid.attach(&button_2, 1, 0, 1, 1);
+    grid.attach(&button_shutdown, 1, 0, 1, 1);
 
-    let button_3 = gtk::Button::with_label("reboot");
-    button_3.connect_clicked(move |_|  match reboot() {
+    let button_reboot = gtk::Button::with_label("reboot");
+    button_reboot.connect_clicked(move |_| match reboot() {
         Ok(_) => println!("reboot, bye!"),
         Err(error) => eprintln!("Failed to reboot: {}", error),
     });
 
-    grid.attach(&button_3, 2, 0, 1, 1);
-    
+    grid.attach(&button_reboot, 2, 0, 1, 1);
+
+    let button_hibernate = gtk::Button::with_label("hibernate");
+    button_hibernate.connect_clicked(move |_| match rew_hibernate() {
+        Ok(_) => println!("Hibernate, bye!"),
+        Err(error) => eprintln!("Failed to hibernate: {}", error),
+    });
+    grid.attach(&button_hibernate, 3, 0, 1, 1);
 
     // Create the quit button and put it into the grid at (0, 1)
     let quit_button = gtk::Button::with_label("Quit");
